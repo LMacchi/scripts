3e4ebb17707cd541cd0be7ed7782b685bc922b3e
LMacchi <lau.macchi@gmail.com>
2017-02-22 22:26:19 -0600

    Resolve modules versions using forge api

---
+++ forge_resolve_version.rb
@@ -0,0 +1,43 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require "net/https"
+require "uri"
+require "json"
+$not_found = Array.new
+mod_list = ARGV[0]
+
+if ARGV.empty?
+  puts "USAGE: #{$0} /path/to/modules/list"
+  exit 1
+end
+
+file = File.open(mod_list, "r") do |fh|
+  fh.each_line do |mod|
+    mod.chomp!
+    url = "https://forgeapi.puppet.com:443/v3/modules/#{mod}"
+    uri = URI.parse(url)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.request_uri, {'User-Agent' => 'test'})
+
+    response = http.request(request)
+    #puts "#{mod} response code is #{response.code}"
+
+    if response.code == '200'
+
+      parsed = JSON.parse(response.body)
+
+      full_name = parsed["slug"]
+      version = parsed["current_release"]["version"]
+
+      puts "mod \'#{full_name}\', \'#{version}\'"
+    else
+      $not_found.push(mod)
+    end
+  end
+
+  puts "\n\nModules not found: #{$not_found}"
+end
