086dafab09d2190ea77721709a6dcfbd55b8258f
LMacchi <lau.macchi@gmail.com>
2017-03-02 00:03:40 -0800

    Now it resolves dependencies

--- forge_resolve_version/README.md
+++ forge_resolve_version/README.md
@@ -1,6 +1,12 @@
-Script created to resolve latest version of forge modules using forge API.
+Script created to resolve latest version of forge modules and their
+dependencies using forge API.
 It returns a Puppetfile_new file in the current directory.
 It notifies on not found and/or decommissioned modules.
+
+Limitations: 
+- Input file needs to be a list of modules
+- Might not resolve all dependencies since the script is not
+recursive
 
 Usage: forge_resolve_version.rb /path/to/modules/list.txt
 
--- forge_resolve_version/forge_resolve_version.rb
+++ forge_resolve_version/forge_resolve_version.rb
@@ -5,7 +5,8 @@ require "uri"
 require "json"
 $not_found = Array.new()
 $decom = Array.new()
-$mods = ""
+$deps = Array.new()
+$mods = Hash.new()
 mod_list = ARGV[0]
 puppetfile = "Puppetfile_new"
 
@@ -15,36 +16,65 @@ if ARGV.empty?
   exit 2
 end
 
+def findModuleData(mod)
+  url = "https://forgeapi.puppet.com:443/v3/modules/#{mod}"
+  uri = URI.parse(url)
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Get.new(uri.request_uri, {'User-Agent' => 'test'})
+
+  response = http.request(request)
+
+  if response.code == '200'
+    parsed = JSON.parse(response.body)
+    name = parsed["slug"]
+    version = parsed["current_release"]["version"]
+    moddeps = parsed["current_release"]["metadata"]["dependencies"]
+
+    return name, version, moddeps
+  end
+end
+
+def processModules(name, version, moddeps)
+  # Is the module decommissioned?
+  if version == '999.999.999' then
+    $decom.push(name)
+  else
+    $mods[name] = version
+  end
+  unless moddeps.empty?
+    moddeps.each do |dep|
+      $deps.push(dep) unless $deps.include? dep
+    end
+  end
+end
+
 # Read modules from the mod_list file and query forge API for data
 file_in = File.open(mod_list, "r") do |fh|
   fh.each_line do |mod|
     mod.chomp!
-    url = "https://forgeapi.puppet.com:443/v3/modules/#{mod}"
-    uri = URI.parse(url)
+    mod.gsub!(/\//,'-')
+    # Method will retusn nil if mod not found
+    name, version, moddeps = findModuleData(mod)
+    if name then
+      processModules(name, version, moddeps)
+    else
+      $not_found.push(mod)
+    end
+  end
+end
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(uri.request_uri, {'User-Agent' => 'test'})
-
-    response = http.request(request)
-    #puts "#{mod} response code is #{response.code}"
-
-    if response.code == '200'
-
-      parsed = JSON.parse(response.body)
-
-      full_name = parsed["slug"]
-      version = parsed["current_release"]["version"]
-      # API returns author-module but r10k needs author/module
-      full_name.gsub!(/-/, '/')
-      # Is the module decommissioned?
-      if version == '999.999.999' then
-        $decom.push(mod)
-      else
-        $mods += "mod \'#{full_name}\', \'#{version}\'\n"
-      end
+# Look for dependencies
+unless $deps.empty?
+  $deps.each do |dep|
+    # Dependencies are returned with a slash, yay for consistency
+    mod = dep["name"].gsub!(/\//, "-")
+    name, version, moddeps = findModuleData(mod)
+    if name then
+      processModules(name, version, moddeps)
     else
       $not_found.push(mod)
     end
@@ -56,7 +86,11 @@ if $mods.empty?
   puts "File #{puppetfile} not generated. No active modules were found on the Forge"
 else
   file_out = File.open(puppetfile, "w") do |fh|
-    fh.puts $mods
+    $mods.each do |mod,ver|
+      # Forgeapi uses dashes, r10k requires slashes
+      name = mod.gsub(/-/, '/')
+      fh.puts "mod '#{name}', '#{ver}'"
+    end
   end
 
   puts "File #{puppetfile} generated."
