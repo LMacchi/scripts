ebb82867fca7821b83e4b2e4c42d6b20a92f4b48
WhatsARanjit <whatsaranjit@gmail.com>
2017-06-13 22:07:53 +0000

    Moved to classes and threading

--- forge_resolve_version/forge_resolve_version.rb
+++ forge_resolve_version/forge_resolve_version.rb
@@ -1,165 +1,176 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 
-require "net/https"
-require "uri"
-require "json"
+require 'net/https'
+require 'uri'
+require 'json'
 require 'optparse'
+require 'puppet'
+require 'r10k/puppetfile'
 
-# Methods
-def findModuleData(mod)
-  puts "Processing module #{mod}"
-  url = "https://forgeapi.puppet.com:443/v3/modules/#{mod}"
-  uri = URI.parse(url)
+Puppet.initialize_settings
 
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+class CmdOptions
+  attr_reader :input, :output
 
-  request = Net::HTTP::Get.new(uri.request_uri, {'User-Agent' => 'test'})
-
-  response = http.request(request)
-
-  if response.code == '200'
-    puts "Module #{mod} found, processing info..."
-    parsed = JSON.parse(response.body)
-    name = parsed["slug"]
-    version = parsed["current_release"]["version"]
-    moddeps = parsed["current_release"]["metadata"]["dependencies"]
-
-    return name, version, moddeps
-  else
-    puts "Module #{mod} not found"
+  def initialize
+    options = parse_options
+    @input  = options[:input]
+    @output = options[:output]
   end
-end
 
-def processModules(name, version, moddeps)
-  # Is the module decommissioned?
-  if version == '999.999.999' then
-    puts "Module #{name} has been decommissioned"
-    $decom.push(name)
-  else
-    $mods[name] = version
-    unless moddeps.empty?
-      moddeps.each do |dep|
-        $deps.push(dep) unless $deps.include? dep
+  private
+
+  def parse_options
+    options = {
+      :input  => "#{Puppet.settings['environmentpath']}/#{Puppet.settings['environment']}/Puppetfile",
+      :output => 'newPuppetfile',
+    }
+    o = OptionParser.new do |opts|
+      opts.banner = "Usage: #{$0}"
+      opts.on('-i FILE', '--input FILE', 'Path to original Puppetfile') do |i|
+        options[:input] = i
+      end
+      opts.on('-o FILE', '--output FILE', 'Path to new and improved Puppetfile') do |o|
+        options[:output] = o
+      end
+      opts.on('-h', '--help', 'Display this help') do
+        puts opts
+        exit
       end
     end
+    o.parse!
+    options
   end
 end
 
-# Get arguments from CLI
-options = {}
-o = OptionParser.new do |opts|
-  opts.banner = "Usage: #{$0}"
-  opts.on('-i [/path/to/original/Puppetfile]', '--input [/path/to/original/Puppetfile', "Path to original Puppetfile") do |i|
-    options[:input] = i
+class ControlRepo
+
+  attr_reader :modules, :mlist, :gitmods
+
+  def initialize(path)
+    puppetfile = R10K::Puppetfile.new(File.dirname(path))
+    puppetfile.load!
+    @modules   = puppetfile.modules
+    @gitmods   = Array.new
+    @mlist     = modules_list
   end
-  opts.on('-o [/path/to/new/Puppetfile]', '--output [/path/to/new/Puppetfile]', "Path to new and improved Puppetfile") do |o|
-    options[:output] = o
-  end
-  opts.on('-h', '--help', 'Display this help') do
-    puts opts
-   $opts = opts
-    exit
-  end
-end
 
-o.parse!
+  def cycle_through_modules(list)
+    forge_data = Hash.new
+    threads    = []
 
-# Validate arguments
-input = options[:input]
-output = options[:output] || 'Puppetfile'
-outdir = File.dirname(output)
+    list.each do |mod|
+      threads << Thread.new do
+        puts "Processing module '#{mod['owner']}-#{mod['name']}'..."
+        res = forge_call(mod['owner'], mod['name'])
 
-
-unless input
-  puts "ERROR: input is a mandatory argument"
-  puts o
-  exit 2
-end
-
-unless File.file?(input)
-  puts "ERROR: #{input} does not exist"
-  puts o
-  exit 2
-end
-
-unless File.directory?(outdir)
-  puts "ERROR: #{outdir} does not exist"
-  puts o
-  exit 2
-end
-
-# Set variables
-$not_found = Array.new()
-$decom = Array.new()
-$deps = Array.new()
-$mods = Hash.new()
-$mods_read = Array.new()
-
-# Read modules from Puppetfile
-file_in = File.open(input, "r") do |fh|
-  fh.each_line do |line|
-    line.chomp!
-    if line =~ /^\s*mod\s+('|")(\w+[\/-]\w+)('|")/ then
-      $mods_read.push($2)
+        if res.code == '200'
+          data = JSON.parse(res.body)
+          forge_data[data['slug']] = {
+            'version' => data['current_release']['version'],
+            'deps'    => data['current_release']['metadata']['dependencies'],
+          }
+          puts "New version: #{data['slug']}-#{data['current_release']['version']}!"
+        end
+      end
     end
+    threads.each { |t| t.join }
+    forge_data
   end
-end
 
-# Search retrieved modules
-$mods_read.each do |mod|
-  mod.gsub!(/\//,'-')
-  # Method will retusn nil if mod not found
-  name, version, moddeps = findModuleData(mod)
-  if name then
-    processModules(name, version, moddeps)
-  else
-    $not_found.push(mod)
-  end
-end
-
-# Look for dependencies
-unless $deps.empty?
-  $deps.each do |dep|
-    # Dependencies are returned with a slash, yay for consistency
-    mod = dep["name"].gsub!(/\//, "-")
-    name, version, moddeps = findModuleData(mod)
-    if name then
-      processModules(name, version, moddeps)
-    else
-      $not_found.push(mod)
+  def add_forge_deps(f)
+  # Don't queue up any new deps from Forge that are
+  # already in the module list.
+    _f = f.map { |mod, data| { 'owner' => mod.split('-')[0], 'name' => mod.split('-')[1], } }
+    check_list(_f, @mlist).flatten.each do |m|
+      f[m['name']] = {
+        'owner' => m['owner'],
+      }
     end
+    f
   end
-end
 
-# return the data found
-if $mods.empty?
-  puts "File #{output} not generated. No active modules were found on the Forge"
-else
-  file_out = File.open(output, "w") do |fh|
-    $mods.each do |mod,ver|
-      # Forgeapi uses dashes, r10k requires slashes
-      name = mod.gsub(/-/, '/')
-      fh.puts "mod '#{name}', '#{ver}'"
+  def gitmod_args
+    @gitmods.map do |m|
+      content = "mod '#{m.name}',\n"
+      content += m.instance_variable_get("@args").map { |k,v| "  :#{k} => '#{v}'" }.join(",\n")
     end
   end
 
-  puts "File #{output} generated."
+  def write_puppetfile(target, list)
+    content  = "# Forge modules\n"
+    content += list.map { |k,v| "mod '#{k}', '#{v['version']}'" }.join("\n")
+    content += "\n\n# Git modules\n"
+    content += gitmod_args.join("\n")
+    begin
+      File.open(target, 'w') { |f| f.write(content) }
+    rescue e
+      fail("Unable to write #{target}: #{e.message}!")
+    end
+  end
 
-  # Clean exit
-  if $not_found.empty? && $decom.empty? then
-    puts "All modules found"
-    exit 0
+  private
+
+  def modules_list
+    list     = Array.new
+    dep_list = Array.new
+
+    @modules.each_entry do |m|
+      if m.owner
+        list << {
+          'owner'   => m.owner,
+          'name'    => m.name,
+          'version' => m.version,
+        }
+        puts "Found module '#{m.owner}-#{m.name} v#{m.version}..."
+      end
+      if m.is_a?(R10K::Module::Forge)
+        dep = m.metadata.dependencies.map do |m|
+          _m    = m['name'].split('/')
+          owner = _m[0]
+          name  = _m[1]
+          {
+            'owner'   => owner,
+            'name'    => name,
+            'version' => nil,
+          }
+        end
+        dep_list << dep
+        dep_list.flatten!
+      else
+        @gitmods << m
+      end
+    end
+    # Pulls deps for all modules, then uniqes it down
+    # to one list.  Checks to see that modules are
+    # not already in the list.
+    list << check_list(dep_list.uniq, list)
+    list.flatten!
+  end
+
+  def check_list(deps, list)
+    deps.map do |m|
+      if list.select { |x| x['owner'] == m['owner'] && x['name'] == m['name']}.empty?
+        puts "Found dependency '#{m['owner']}-#{m['name']}'..."
+        m
+      end
+    end.compact
+  end
+
+  def forge_call(owner, mod)
+    url              = "https://forgeapi.puppet.com:443/v3/modules/#{owner}-#{mod}"
+    uri              = URI.parse(url)
+    http             = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl     = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    req              = Net::HTTP::Get.new(uri.request_uri, {'User-Agent' => 'test'})
+    res              = http.request(req)
   end
 end
 
-# Errors found
-if ! $not_found.empty? then
-  puts "Modules not found: #{$not_found}"
-end
-if ! $decom.empty? then
-  puts "Modules decommissioned: #{$decom}"
-end
+opts        = CmdOptions.new
+controlrepo = ControlRepo.new(opts.input)
+forge_data  = controlrepo.cycle_through_modules(controlrepo.mlist)
+f_data_deps = controlrepo.add_forge_deps(forge_data)
 
-exit 2
+controlrepo.write_puppetfile(opts.output, f_data_deps)
